7db8803f9901e93a197d35611ba2a39764e3c3c1
minping111111 <minping@bupt.edu.cn>
2017-05-19 20:49:38 +0800

    Update list.go

--- list.go
+++ list.go
@@ -31,13 +31,13 @@ func printList(head *node) *node {
 }
 
 func main() {
-	printList(node0)
-	newHead := listReverse(node0, nil)
+	// printList(node0)
+	newHead := listReverse_for(node0)
 	printList(newHead)
 }
 
 //递归实现链表反转，传入头结点，返回新的头结点
-func listReverse(current, prev *node) *node {
+func listReverse_recursive(current, prev *node) *node {
 	if current == nil || current.next == nil {
 		current.next = prev
 		return current
@@ -46,6 +46,24 @@ func listReverse(current, prev *node) *node {
 	tempNext := current.next
 	current.next = prev
 	prev = current
-	return listReverse(tempNext, prev)
+	return listReverse_recursive(tempNext, prev)
+
+}
+//循环实现链表反转，传入头结点，返回新的头结点
+func listReverse_for(current *node) *node {
+	var prev *node
+	for current != nil {
+		if current.next == nil {
+			current.next = prev
+			break
+		} else {
+			tempnext := current.next
+			current.next = prev
+			prev = current
+			current = tempnext
+		}
+
+	}
+	return current
 
 }
