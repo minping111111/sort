48343b7a1707f7125bd7dc8c49ed16257c05132a
minping111111 <minping@bupt.edu.cn>
2017-06-03 21:28:33 +0800

    Update leetcode3.go

--- leetcode3.go
+++ leetcode3.go
@@ -22,8 +22,22 @@ func main() {
 	// fmt.Println(res)
 	// res := gasStation([]int{1, 3, 5, 7, 9}, []int{4, 4, 5, 9, 6})
 	// fmt.Println(res)
-	res := candy([]float64{1.1, 2.3, 2.1, 5.3, 9.2, 4.5, 3.6})
+	// res := candy([]float64{1.1, 2.3, 2.1, 5.3, 9.2, 4.5, 3.6})
+	// fmt.Println(res)
+
+	// res := findSingleNumII([]int{1, 2, 3, 4, 5, 5, 4, 3, 1})
+	// fmt.Println(res)
+	// res := AddTwoNumbers([]int{2, 4, 3}, []int{5, 6, 4})
+	// fmt.Println(res)
+	// res, err := reverseLinkedListII([]int{1, 2, 3, 4, 5}, 2, 4)
+	// if err == nil {
+	// 	fmt.Println(res)
+	// } else {
+	// 	fmt.Println(err)
+	// }
+	res := partitionList([]int{1, 4, 3, 2, 5, 2}, 3)
 	fmt.Println(res)
+
 }
 
 //将n*n矩阵顺时针旋转90度
@@ -191,41 +205,131 @@ func candy(ratingVal []float64) (sum int) {
 	if leng < 1 {
 		return 0
 	}
-	candyDetail :=[leng]int{}
-	min := 0
-	for i := min; i < leng; i++ {
-		if ratingVal[i] < ratingVal[min] {
-			min = i
+	candyDetail := []int{}
+	//每个小孩给一颗糖
+	ii := 0
+	for ii < leng {
+		candyDetail = append(candyDetail, 1)
+		ii++
+	}
+	// 从左到右遍历，如果第i+1个小孩的等级比第i个小孩的等级要高，则第i+1个小孩的糖火数是第i个小孩的糖果数+1
+	for i := 0; i < leng-1; i++ {
+		if ratingVal[i+1] > ratingVal[i] {
+			candyDetail[i+1] = candyDetail[i] + 1
 		}
 	}
-	//最少分数的小孩给一颗糖果
-	candyDetail[min] = 1
-	if min > 0 {
-		indleft := 1
-		for i := min - 1; i > 0; i-- {
-			if ratingVal[i] > ratingVal[i+1] {
-				indleft += 1
-				sum += indleft
-			} else {
-				//Todo
-				sum += candy(ratingVal[:])
-			}
-
+	// 从右到左遍历，如果第j-1个小孩的等级比第j个小孩的等级要高，且第j-1个小孩的糖不比第j个小孩的糖要多，则第j-1个小孩的糖火数是第j个小孩的糖果数+1
+	for j := leng - 1; j > 0; j-- {
+		if ratingVal[j-1] > ratingVal[j] && candyDetail[j-1] <= candyDetail[j] {
+			candyDetail[j-1] = candyDetail[j] + 1
 		}
 	}
 
-	if min < leng-1 {
-		indright := 1
-		for j := min + 1; j < leng; j++ {
-			if ratingVal[j] > ratingVal[j-1] {
-				indright += 1
-				sum += indright
-			} else {
-				//Todo
-				res, ind := candy(ratingVal[:])
-			}
-
-		}
+	for k := 0; k < leng; k++ {
+		sum += candyDetail[k]
 	}
 	return sum
+}
+
+//给一个数组，除了一个数之外，其他数都出现了两次，找出这个数
+func findSingleNum1(source []int) []int {
+	hashm := make(map[int]bool)
+	for _, val := range source {
+		if _, ok := hashm[val]; ok {
+			delete(hashm, val)
+		} else {
+			hashm[val] = true
+		}
+	}
+	res := []int{}
+	for k, _ := range hashm {
+		res = append(res, k)
+	}
+	return res //这个return-1只是废话，但是得满足函数的返回值类型
+}
+
+//根据两个相同数的异或结果为0,0和x的异或结果为x，依次将每个元素异或一次，最终结果就是那个落单的元素
+func findSingleNum2(source []int) int {
+	num := 0
+	for i := 0; i < len(source); i++ {
+		num ^= source[i]
+	}
+
+	return num
+}
+
+//给一个数组，除了一个数之外，其他数都出现了三次，找出这个数
+func findSingleNumII1(source []int) []int {
+	hashm := make(map[int]int)
+	for _, val := range source {
+		if _, ok := hashm[val]; ok {
+			hashm[val] += 1
+		} else {
+			hashm[val] = 1
+		}
+	}
+	res := []int{}
+	for k, v := range hashm {
+		if v != 3 {
+			res = append(res, k)
+		}
+
+	}
+	return res
+}
+
+// //位运算,就是3->11，如果3出现了3次，则相加后11变成了33(二进制位运算不进位)，就是二进制位数的和也是3的倍数。这样就可以剔除掉出现3次的那些数
+// func findSingleNumII2(source []int) []int {
+
+// }
+
+/*
+给定两个链表，返回两个链表的和
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+*/
+func AddTwoNumbers(source1, source2 []int) []int {
+	leng := len(source1)
+	newSource := []int{}
+	extra := 0
+	for i := 0; i < leng; i++ {
+		sum := source1[i] + source2[i] + extra
+		if sum > 9 {
+			extra = sum / 10
+			sum = sum % 10
+		}
+		newSource = append(newSource, sum)
+	}
+	return newSource
+}
+
+/*
+Reverse a linked list from position m to n. Do it in-place and in one-pass.
+For example: Given 1->2->3->4->5->nullptr, m = 2 and n = 4,
+return 1->4->3->2->5->nullptr.
+Note: Given m, n satisfy the following condition: 1 ≤ m ≤ n ≤ length of list
+*/
+func reverseLinkedListII(source []int, m, n int) ([]int, error) {
+	leng := len(source)
+	if !(m >= 1 && m <= n && n <= leng) {
+		return nil, errors.New(`参数错误`)
+	}
+	// fmt.Println(source)
+	for i := m - 1; i < (m+n-2)/2; i++ {
+		source[i], source[m+n-2-i] = source[m+n-2-i], source[i]
+	}
+	return source, nil
+}
+
+/*
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater
+than or equal to x.
+You should preserve the original relative order of the nodes in each of the two partitions.
+For example, Given 1->4->3->2->5->2 and x = 3, return 1->2->2->4->3->5.
+*/
+func partitionList(source []int, param int) []int {
+	ind := 0
+	for i := 0; i < len(source); i++ {
+
+	}
 }
