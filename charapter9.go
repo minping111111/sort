d0f22d31b351d3b473f05a97fba7403079bdc612
minping111111 <minping@bupt.edu.cn>
2017-08-08 14:25:24 +0800

    完成leetcode第九章广度优先搜索LBS算法第一题
    完成leetcode第九章广度优先搜索LBS算法第一题及其他结构调整

--- charapter9.go
+++ charapter9.go
@@ -3,8 +3,9 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
-	// "strings"
+	"strings"
 )
 
 var tu = map[string][]string{
@@ -26,17 +27,15 @@ var maze = [][]int{
 }
 
 func main() {
-	exist, path := shortRoad([]int{2, 2}, []int{4, 4})
+	exist, path := baseTu("v0", "v6")
+	// exist, path := shortRoad([]int{0, 2}, []int{4, 4})
+	// exist, path := WordLadder("cog", "cog")
 	if exist {
 		fmt.Println(path)
 	} else {
 		fmt.Println("the shortest path does not exists...")
 	}
 }
-
-var gray = []string{}
-var black = make(map[string]bool)
-var pathRecord = make(map[string]string)
 
 /*
 广度(宽度)优先搜索，见http://blog.csdn.net/raphealguo/article/details/7523411
@@ -52,16 +51,17 @@ func baseTu(vs, vd string) (bool, []string) {
 	if vs == vd {
 		return true, []string{vs}
 	}
+	gray := []string{}
+	pathRecord := make(map[string]string)
 	gray = append(gray, vs)
 	for len(gray) > 0 && gray[0] != vd {
 		toB := gray[0]
 		if toB == vd {
 			break
 		}
-		black[toB] = true
 		gray = gray[1:]
 		for i := 0; i < len(tu[toB]); i++ {
-			if _, ok := black[tu[toB][i]]; !ok {
+			if _, ok := pathRecord[tu[toB][i]]; !ok {
 				gray = append(gray, tu[toB][i])
 				if _, ok2 := pathRecord[tu[toB][i]]; !ok2 {
 					pathRecord[tu[toB][i]] = toB //这里记录路径，key为节点，val为从哪个点走到这个节点，要先判断pathRecord里面是否存在此key，存在则不能变，否则路径会被覆盖。。。
@@ -104,9 +104,80 @@ Note:
 • All words have the same length.
 • All words contain only lowercase alphabetic characters.
 **/
-// func WordLadder() {
 
-// }
+var dictArray = []string{"hot", "dot", "dog", "lot", "log"}
+
+var dict = map[string][]string{
+	"hot": []string{"dot", "lot"},
+	"dot": []string{"hot", "dog", "lot"},
+	"dog": []string{"dot", "log"},
+	"lot": []string{"hot", "dot", "log"},
+	"log": []string{"dog", "lot"},
+}
+
+//"hit", "cog"
+func WordLadder(vs, vd string) (bool, []string) {
+	if vs == vd {
+		return true, []string{vs}
+	}
+	//先建立V和E(点和边)的关系
+	addVnode(vs)
+	addVnode(vd)
+	gray := []string{}
+	// black := make(map[string]string)
+	pathRecord := map[string]string{
+		vs: "",
+	}
+
+	gray = append(gray, vs)
+	for len(gray) > 0 && gray[0] != vd {
+		toB := gray[0]
+		for _, v := range dict[toB] {
+			if _, ok := pathRecord[v]; !ok {
+				pathRecord[v] = toB
+				gray = append(gray, v)
+			}
+		}
+		gray = gray[1:]
+	}
+
+	if len(gray) == 0 || gray[0] != vd {
+		return false, []string{}
+	}
+	last := vd
+	pathArray := []string{vd}
+	for pathRecord[last] != vs {
+		pathArray = append(pathArray, pathRecord[last])
+		last = pathRecord[last]
+	}
+	pathArray = append(pathArray, vs)
+	for i := 0; i < len(pathArray)/2; i++ {
+		pathArray[i], pathArray[len(pathArray)-1-i] = pathArray[len(pathArray)-1-i], pathArray[i]
+	}
+	return true, pathArray
+}
+
+func addVnode(vs string) {
+	if _, ok := dict[vs]; !ok {
+		dict[vs] = []string{}
+		for _, v := range dictArray {
+			vArr := strings.Split(v, "")
+			vsArr := strings.Split(vs, "")
+			sort.Strings(vArr)
+			sort.Strings(vsArr)
+			num := 0
+			for i := 0; i < len(vArr); i++ {
+				if vArr[i] == vsArr[i] {
+					num += 1
+				}
+			}
+			if num == (len(vArr) - 1) {
+				dict[v] = append(dict[v], vs)
+				dict[vs] = append(dict[vs], v)
+			}
+		}
+	}
+}
 
 /*
 《迷宫问题》
@@ -125,7 +196,6 @@ func shortRoad(vs []int, vd []int) (bool, [][]int) {
 		return true, [][]int{vs}
 	}
 	gray := [][]int{}
-	black := make(map[string]bool)
 	pathRecord := make(map[string][]int)
 
 	gray = append(gray, vs)
@@ -139,7 +209,6 @@ func shortRoad(vs []int, vd []int) (bool, [][]int) {
 		if toB[0] == vd[0] && toB[1] == vd[1] {
 			break
 		}
-		black[strconv.Itoa(toB[0])+","+strconv.Itoa(toB[1])] = true
 		temp := [][]int{}
 		if toB[0]-1 >= 0 && toB[0]-1 < len(maze) {
 			temp = append(temp, []int{toB[0] - 1, toB[1]})
@@ -155,7 +224,7 @@ func shortRoad(vs []int, vd []int) (bool, [][]int) {
 		}
 		for _, v := range temp {
 			if maze[v[0]][v[1]] == 0 || (v[0] == vd[0] && v[1] == vd[1]) {
-				if _, ok := black[strconv.Itoa(v[0])+","+strconv.Itoa(v[1])]; !ok {
+				if _, ok := pathRecord[strconv.Itoa(v[0])+","+strconv.Itoa(v[1])]; !ok {
 					gray = append(gray, v)
 					if _, ok2 := pathRecord[strconv.Itoa(v[0])+","+strconv.Itoa(v[1])]; !ok2 {
 						pathRecord[strconv.Itoa(v[0])+","+strconv.Itoa(v[1])] = toB
@@ -184,14 +253,3 @@ func shortRoad(vs []int, vd []int) (bool, [][]int) {
 	return true, pathArray
 
 }
-
-// func (sa []int) join() string {
-// 	if len(bb) == 0 {
-// 		return ""
-// 	}
-// 	var res []string
-// 	for _, v := range sa {
-// 		res = append(res, strconv.Itoa(v))
-// 	}
-// 	return true, strings.Join(res, ",")
-// }
